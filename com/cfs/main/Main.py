3d6877e5e685a79f70b0ca215ef20feb21266342
csap0422 <Christopher543sap@gmail.com>
2018-08-31 12:10:22 -0500

    Commit

--- com/cfs/main/Main.py
+++ com/cfs/main/Main.py
@@ -1,33 +1,48 @@
 import mechanicalsoup
 import os
 
-global downloaded_links = []
+downloaded_urls = []
 
 # downloads a file with the url as a name
 def download_file(url, browser):
     path = "downloads\\" + url[:-1].replace("https://", "").replace("/", "+") + ".html"
+    for i in range(0, len(path)):
+        if path[i] == "+":
+            path = path[i:]
+        else:
+            break
     file = open(path, "w+")
     file.close()
+    if "https://" not in browser.get_url() and "http://" not in browser.get_url():
+        browser.open("https://" + browser.get_url())
+    elif "https:" not in browser.get_url() and "http:" not in browser.get_url():
+        browser.open("https" + browser.get_url())
     browser.download_link(file=path)
+    downloaded_urls.append(url)
 
-def download_all_links(function_browser):
+def downloaded(url):
+    return url in downloaded_urls
+
+def download_all_links(initial_url, function_browser):
     # get all links
-    links = [function_browser.get_url()]
+    links = [function_browser.get_url()] # add initial url
     for x in function_browser.get_current_page().find_all('a'):
         links.append(x.get('href'))
-    # only use links that have github.com in url
+    print(links)
+    # only use links that have initial url in url
     temp = set() # set to remove duplicates
     for x in links:
-        if "github.com" in x:
+        if initial_url.replace("https://www", "") in x:
             temp.add(x)
     links = temp
     print(links)
     # download all pages linked on this page
     for x in links:
-        download_file(str(x))
+        if not downloaded(x):
+            download_file(str(x), function_browser)
 
 def run():
-    initial_url = "https://github.com"
+    initial_url = "https://www.wikipedia.org/"
 
     # create browser object
     browser = mechanicalsoup.StatefulBrowser()
@@ -35,7 +50,8 @@ def run():
     os.chdir("..\\..\\..")
     # open starting page
     browser.open(initial_url)
-    download_all_links(browser)
+    download_all_links(initial_url, browser)
 
 if __name__ == '__main__':
     run()
+    print(downloaded_urls)
