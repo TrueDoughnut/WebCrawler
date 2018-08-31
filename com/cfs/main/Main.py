554d1aaaae23e37c74d7399f7bc19b773b09f5f0
csap0422 <Christopher543sap@gmail.com>
2018-08-31 11:44:04 -0500

    Commit

--- com/cfs/main/Main.py
+++ com/cfs/main/Main.py
@@ -1,25 +1,41 @@
 import mechanicalsoup
 import os
 
-# create browser object
-browser = mechanicalsoup.StatefulBrowser()
-# change directory to access downloads folder
-os.chdir("..\\..\\..")
+global downloaded_links = []
 
 # downloads a file with the url as a name
-def download_file(url):
+def download_file(url, browser):
     path = "downloads\\" + url[:-1].replace("https://", "").replace("/", "+") + ".html"
     file = open(path, "w+")
     file.close()
     browser.download_link(file=path)
 
-# open starting page
-browser.open("https://github.com/")
-download_file(browser.get_url())
+def download_all_links(function_browser):
+    # get all links
+    links = [function_browser.get_url()]
+    for x in function_browser.get_current_page().find_all('a'):
+        links.append(x.get('href'))
+    # only use links that have github.com in url
+    temp = set() # set to remove duplicates
+    for x in links:
+        if "github.com" in x:
+            temp.add(x)
+    links = temp
+    print(links)
+    # download all pages linked on this page
+    for x in links:
+        download_file(str(x))
 
-# get all links
-links = []
-for x in browser.get_current_page().find_all('a'):
-    links.append(x.get('href'))
+def run():
+    initial_url = "https://github.com"
 
+    # create browser object
+    browser = mechanicalsoup.StatefulBrowser()
+    # change directory to access downloads folder
+    os.chdir("..\\..\\..")
+    # open starting page
+    browser.open(initial_url)
+    download_all_links(browser)
 
+if __name__ == '__main__':
+    run()
